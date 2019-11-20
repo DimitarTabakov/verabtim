fecbb0501726a0d509e8029167b3720bb3137a28
DimitarTabakov <dimiturtabakov@gmail.com>
2019-11-20 15:02:52 +0200

    Update verbatim.sql

--- SQL Server/verbatim.sql
+++ SQL Server/verbatim.sql
@@ -8,88 +8,88 @@ AS
 BEGIN
     RETURN
     case
-/* Уникални, малки числа */
+/* РЈРЅРёРєР°Р»РЅРё, РјР°Р»РєРё С‡РёСЃР»Р° */
     when (@num between 0 and 19) then
       case @a when 1 then
 		  case @num
-		  when 1 then 'Един' when 6 then 'Шест' when 11 then 'Единадесет' when 16 then 'Шестнадесет'
-		  when 2 then 'Два' when 7 then 'Седем' when 12 then 'Дванадесет' when 17 then 'Седемнадесет'
-		  when 3 then 'Три' when 8 then 'Осем' when 13 then 'Тринадесет' when 18 then 'Осемнадесет'
-		  when 4 then 'Четири' when 9 then 'Девет' when 14 then 'Четиринадесет' when 19 then 'Деветнадесет'
-		  when 5 then 'Пет' when 10 then 'Десет' when 15 then 'Петнадесет' when 0 then 'Нула'
+		  when 1 then 'Р•РґРёРЅ'   when 6  then 'РЁРµСЃС‚'  when 11 then 'Р•РґРёРЅР°РґРµСЃРµС‚'    when 16 then 'РЁРµСЃС‚РЅР°РґРµСЃРµС‚'
+		  when 2 then 'Р”РІР°'    when 7  then 'РЎРµРґРµРј' when 12 then 'Р”РІР°РЅР°РґРµСЃРµС‚'    when 17 then 'РЎРµРґРµРјРЅР°РґРµСЃРµС‚'
+		  when 3 then 'РўСЂРё'    when 8  then 'РћСЃРµРј'  when 13 then 'РўСЂРёРЅР°РґРµСЃРµС‚'    when 18 then 'РћСЃРµРјРЅР°РґРµСЃРµС‚'
+		  when 4 then 'Р§РµС‚РёСЂРё' when 9  then 'Р”РµРІРµС‚' when 14 then 'Р§РµС‚РёСЂРёРЅР°РґРµСЃРµС‚' when 19 then 'Р”РµРІРµС‚РЅР°РґРµСЃРµС‚'
+		  when 5 then 'РџРµС‚'    when 10 then 'Р”РµСЃРµС‚' when 15 then 'РџРµС‚РЅР°РґРµСЃРµС‚'    when 0 then 'РќСѓР»Р°'
 		  END
-      else 
+      else
 		  case @num
-		  when 1 then 'Една' when 6 then 'Шест' when 11 then 'Единадесет' when 16 then 'Шестнадесет'
-		  when 2 then 'Две' when 7 then 'Седем' when 12 then 'Дванадесет' when 17 then 'Седемнадесет'
-		  when 3 then 'Три' when 8 then 'Осем' when 13 then 'Тринадесет' when 18 then 'Осемнадесет'
-		  when 4 then 'Четири' when 9 then 'Девет' when 14 then 'Четиринадесет' when 19 then 'Деветнадесет'
-		  when 5 then 'Пет' when 10 then 'Десет' when 15 then 'Петнадесет' when 0 then 'Нула'
+		  when 1 then 'Р•РґРЅР°'   when 6 then 'РЁРµСЃС‚'   when 11 then 'Р•РґРёРЅР°РґРµСЃРµС‚'    when 16 then 'РЁРµСЃС‚РЅР°РґРµСЃРµС‚'
+		  when 2 then 'Р”РІРµ'    when 7 then 'РЎРµРґРµРј'  when 12 then 'Р”РІР°РЅР°РґРµСЃРµС‚'    when 17 then 'РЎРµРґРµРјРЅР°РґРµСЃРµС‚'
+		  when 3 then 'РўСЂРё'    when 8 then 'РћСЃРµРј'   when 13 then 'РўСЂРёРЅР°РґРµСЃРµС‚'    when 18 then 'РћСЃРµРјРЅР°РґРµСЃРµС‚'
+		  when 4 then 'Р§РµС‚РёСЂРё' when 9 then 'Р”РµРІРµС‚'  when 14 then 'Р§РµС‚РёСЂРёРЅР°РґРµСЃРµС‚' when 19 then 'Р”РµРІРµС‚РЅР°РґРµСЃРµС‚'
+		  when 5 then 'РџРµС‚'    when 10 then 'Р”РµСЃРµС‚' when 15 then 'РџРµС‚РЅР°РґРµСЃРµС‚'    when 0 then 'РќСѓР»Р°'
 		  END
 	   end
-      /* Десетици */
+      /* Р”РµСЃРµС‚РёС†Рё */
     when (@num between 20 and 90) and (@num % 10 = 0) then
       case @num
-      when 20 then 'Двадесет' when 60 then 'Шестдесет'
-      when 30 then 'Тридесет' when 70 then 'Седемдесет'
-      when 40 then 'Четиридесет' when 80 then 'Осемдесет'
-      when 50 then 'Петдесет' when 90 then 'Деветдесет'
+      when 20 then 'Р”РІР°РґРµСЃРµС‚'    when 60 then 'РЁРµСЃС‚РґРµСЃРµС‚'
+      when 30 then 'РўСЂРёРґРµСЃРµС‚'    when 70 then 'РЎРµРґРµРјРґРµСЃРµС‚'
+      when 40 then 'Р§РµС‚РёСЂРёРґРµСЃРµС‚' when 80 then 'РћСЃРµРјРґРµСЃРµС‚'
+      when 50 then 'РџРµС‚РґРµСЃРµС‚'    when 90 then 'Р”РµРІРµС‚РґРµСЃРµС‚'
       end
-    /* Между десетиците */
-    when (@num between 20 and 99) and (@num % 10 != 0) then (select dbo.words(((@num/ 10) * 10),@a))+ ' и ' + (select dbo.words((@num % 10),@a))
-    
-    /* Стотици, кръгли */
+    /* РњРµР¶РґСѓ РґРµСЃРµС‚РёС†РёС‚Рµ */
+    when (@num between 20 and 99) and (@num % 10 != 0) then (select dbo.words(((@num/ 10) * 10),@a))+ ' Рё ' + (select dbo.words((@num % 10),@a))
+
+    /* РЎС‚РѕС‚РёС†Рё, РєСЂСЉРіР»Рё */
 	when (@num between 100 and 900) and (@num % 100 = 0) then
 	  case @num
-	  when 100 then 'Сто' when 600 then 'Шестстотин'
-	  when 200 then 'Двеста' when 700 then 'Седемстотин'
-	  when 300 then 'Триста' when 800 then 'Осемстотин'
-	  when 400 then 'Четиристотин' when 900 then 'Деветстотин'
-	  when 500 then 'Петстотин'
+	  when 100 then 'РЎС‚Рѕ'          when 600 then 'РЁРµСЃС‚СЃС‚РѕС‚РёРЅ'
+	  when 200 then 'Р”РІРµСЃС‚Р°'       when 700 then 'РЎРµРґРµРјСЃС‚РѕС‚РёРЅ'
+	  when 300 then 'РўСЂРёСЃС‚Р°'       when 800 then 'РћСЃРµРјСЃС‚РѕС‚РёРЅ'
+	  when 400 then 'Р§РµС‚РёСЂРёСЃС‚РѕС‚РёРЅ' when 900 then 'Р”РµРІРµС‚СЃС‚РѕС‚РёРЅ'
+	  when 500 then 'РџРµС‚СЃС‚РѕС‚РёРЅ'
 	  end
-	/* Стотици, не кръгли */
+	/* РЎС‚РѕС‚РёС†Рё, РЅРµ РєСЂСЉРіР»Рё */
 	when (@num between 100 and 999) and (@num % 100 != 0) then
 	case
-	/* Стотици, които завършват на уникално число, или на кръгло 10 */
-	  when (@num % 100 between 1 and 19) or (@num % 10 = 0) then (select dbo.words(((@num / 100) * 100),@a)) + ' и ' + (select dbo.words((@num % 100),@a))
-	/* Стотици, останали */
+	/* РЎС‚РѕС‚РёС†Рё, РєРѕРёС‚Рѕ Р·Р°РІСЉСЂС€РІР°С‚ РЅР° СѓРЅРёРєР°Р»РЅРѕ С‡РёСЃР»Рѕ, РёР»Рё РЅР° РєСЂСЉРіР»Рѕ 10 */
+	  when (@num % 100 between 1 and 19) or (@num % 10 = 0) then (select dbo.words(((@num / 100) * 100),@a)) + ' Рё ' + (select dbo.words((@num % 100),@a))
+	/* РЎС‚РѕС‚РёС†Рё, РѕСЃС‚Р°РЅР°Р»Рё */
 	  else (select dbo.words(((@num / 100) * 100),@a)) + ' ' + (select dbo.words((@num % 100),@a))
 	  end
 	/* 1000 */
-    when @num = 1000 then 'Хиляда'
-    /* Между 1000 и 2000 */
+    when @num = 1000 then 'РҐРёР»СЏРґР°'
+    /* РњРµР¶РґСѓ 1000 Рё 2000 */
     when @num between 1001 and 1999 then
     case
-    /* Които завършват на кръгло 100, кръгло 10 или уникално число */
-       when (@num % 1000 between 1 and 19) or (@num % 1000 < 100 and @num % 10 = 0) or (@num % 100 = 0) then 'Хиляда' + ' и ' + (select dbo.words((@num % 1000),@a))
-    /* Останалите */
-	   else 'Хиляда' + ' ' + (select dbo.words((@num % 1000),@a))
+    /* РљРѕРёС‚Рѕ Р·Р°РІСЉСЂС€РІР°С‚ РЅР° РєСЂСЉРіР»Рѕ 100, РєСЂСЉРіР»Рѕ 10 РёР»Рё СѓРЅРёРєР°Р»РЅРѕ С‡РёСЃР»Рѕ */
+       when (@num % 1000 between 1 and 19) or (@num % 1000 < 100 and @num % 10 = 0) or (@num % 100 = 0) then 'РҐРёР»СЏРґР°' + ' Рё ' + (select dbo.words((@num % 1000),@a))
+    /* РћСЃС‚Р°РЅР°Р»РёС‚Рµ */
+	   else 'РҐРёР»СЏРґР°' + ' ' + (select dbo.words((@num % 1000),@a))
 	   end
-      /* Между 2000 и милион-без-едно */
+      /* РњРµР¶РґСѓ 2000 Рё РјРёР»РёРѕРЅ-Р±РµР·-РµРґРЅРѕ */
     when @num between 2000 and 999999 then
        case
-    /* Кръгли хиляди */
-       when @num % 1000 = 0 then (select dbo.words((@num / 1000),0)) + ' Хиляди '
-    /* Които завършват на кръгло 100, кръгло 10 или уникално число */
-       when (@num % 1000 != 0) and ((@num % 1000 between 1 and 19) or (@num % 1000 < 100 and @num % 10 = 0) or (@num % 100 = 0)) then (select dbo.words((@num / 1000),0)) + ' Хиляди и ' + (select dbo.words((@num % 1000),@a))
-    /* Останалите */
-       else (select dbo.words((@num / 1000),0)) + ' Хиляди ' + (select dbo.words((@num % 1000),@a))
+    /* РљСЂСЉРіР»Рё С…РёР»СЏРґРё */
+       when @num % 1000 = 0 then (select dbo.words((@num / 1000),0)) + ' РҐРёР»СЏРґРё '
+    /* РљРѕРёС‚Рѕ Р·Р°РІСЉСЂС€РІР°С‚ РЅР° РєСЂСЉРіР»Рѕ 100, РєСЂСЉРіР»Рѕ 10 РёР»Рё СѓРЅРёРєР°Р»РЅРѕ С‡РёСЃР»Рѕ */
+       when (@num % 1000 != 0) and ((@num % 1000 between 1 and 19) or (@num % 1000 < 100 and @num % 10 = 0) or (@num % 100 = 0)) then (select dbo.words((@num / 1000),0)) + ' РҐРёР»СЏРґРё Рё ' + (select dbo.words((@num % 1000),@a))
+    /* РћСЃС‚Р°РЅР°Р»РёС‚Рµ */
+       else (select dbo.words((@num / 1000),0)) + ' РҐРёР»СЏРґРё ' + (select dbo.words((@num % 1000),@a))
        end
-    /* Милион */
-   when @num = 1000000 then 'Един Милион'
-   /* Между един и два милиона */
+    /* РњРёР»РёРѕРЅ */
+   when @num = 1000000 then 'Р•РґРёРЅ РњРёР»РёРѕРЅ'
+   /* РњРµР¶РґСѓ РµРґРёРЅ Рё РґРІР° РјРёР»РёРѕРЅР° */
    when @num between 1000001 and 1999999 then
      case
-   /* Които завършват на кръгли или уникални */
-      when (@num % 1000000 between 1 and 19) or ((@num % 1000000) % 10 = 0 and (@num % 1000000) < 100) or (@num % 100 = 0) then (select dbo.words(((@num / 1000000) * 1000000),@a)) + ' и ' + (select dbo.words((@num % 1000000),@a))
+   /* РљРѕРёС‚Рѕ Р·Р°РІСЉСЂС€РІР°С‚ РЅР° РєСЂСЉРіР»Рё РёР»Рё СѓРЅРёРєР°Р»РЅРё */
+      when (@num % 1000000 between 1 and 19) or ((@num % 1000000) % 10 = 0 and (@num % 1000000) < 100) or (@num % 100 = 0) then (select dbo.words(((@num / 1000000) * 1000000),@a)) + ' Рё ' + (select dbo.words((@num % 1000000),@a))
       else (select dbo.words(((@num / 1000000) * 1000000),@a)) + ' ' + (select dbo.words((@num % 1000000),@a))
      end
-   /* Два милиона */
-   when @num = 2000000 then 'Два Милиона'
-   /* Между 2 и 3 милиона */
+   /* Р”РІР° РјРёР»РёРѕРЅР° */
+   when @num = 2000000 then 'Р”РІР° РњРёР»РёРѕРЅР°'
+   /* РњРµР¶РґСѓ 2 Рё 3 РјРёР»РёРѕРЅР° */
    when @num between 2000001 and 2999999 then (select dbo.words(((@num / 1000000) * 1000000),@a)) + ' ' + (select dbo.words((@num % 1000000),@a))
-   /* Между 3 милиона и милиард-без-едно */
-   when @num between 3000000 and 999999999 then (select dbo.words((@num / 1000000),@a)) + ' Милиона ' + (select dbo.words((@num % 1000000),@a))
-      
+   /* РњРµР¶РґСѓ 3 РјРёР»РёРѕРЅР° Рё РјРёР»РёР°СЂРґ-Р±РµР·-РµРґРЅРѕ */
+   when @num between 3000000 and 999999999 then (select dbo.words((@num / 1000000),@a)) + ' РњРёР»РёРѕРЅР° ' + (select dbo.words((@num % 1000000),@a))
+
    END
 END
